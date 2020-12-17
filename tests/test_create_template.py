9edcb6a5690cad267ae74520ef4b62f087bec851
Bartosz Telenczuk <bartosz.telenczuk@gmail.com>
2020-12-17 11:30:25 +0100

    installing plugin with quetz plugin command

--- tests/test_create_template.py
+++ tests/test_create_template.py
@@ -7,44 +7,43 @@ import subprocess
 from quetz import cli as qcli
 
 
-
-def quetz_cmd(cmd, *args):
-    """Run the tox suite of the newly created plugin."""
+def shell_cmd(*args):
     try:
-        output = subprocess.check_output([
-            "quetz",
-            cmd,
-        ] + list(args), stderr=subprocess.STDOUT)
+        output = subprocess.check_output(
+                list(args), stderr=subprocess.STDOUT)
     except subprocess.CalledProcessError as e:
-        pytest.fail(e)
+        pytest.fail(e.output.decode())
     return output.decode()
 
 
-def test_run_tests(plugin):
+def quetz_cmd(cmd, *args):
     """Run the tox suite of the newly created plugin."""
-    try:
-        subprocess.check_call([
-            "pytest",
-            plugin,
-        ])
-    except subprocess.CalledProcessError as e:
-        pytest.fail(e)
+    return shell_cmd("quetz", cmd, *args)
 
+
+def test_run_tests(plugin_dir, install_plugin):
+    """Run the tox suite of the newly created plugin."""
+    shell_cmd("pytest", plugin_dir)
 
 @pytest.fixture
-def quetz_deployment():
+def deploy_dir():
+
     deploy_dir = Path(tempfile.mkdtemp()) / 'deployment'
+    return deploy_dir
+
+@pytest.fixture
+def quetz_deployment(deploy_dir):
 
     quetz_cmd("create", deploy_dir, '--create-conf')
 
     yield deploy_dir
 
 
-def test_initialize_migrations(plugin, quetz_deployment, capsys):
-    deploy_dir = quetz_deployment
+@pytest.mark.parametrize("use_quetz", [False])
+def test_initialize_migrations(install_plugin, quetz_deployment, deploy_dir, capsys, plugin_dir):
 
     quetz_cmd("make-migrations", deploy_dir, '--plugin', "quetz-foo_bar", "--message", "initial foo_bar revision", "--initialize")
-    plugin_dir = Path(plugin)
+    plugin_dir = Path(plugin_dir)
     version_location = plugin_dir / "quetz_foo_bar" / "migrations" / "versions"
     migration_scripts = list(version_location.glob("*initial_foo_bar_revision.py"))
     assert migration_scripts
@@ -54,7 +53,11 @@ def test_initialize_migrations(plugin, quetz_deployment, capsys):
     assert "initial foo_bar revision" in output
 
 @pytest.fixture
-def plugin(cookies):
+def use_quetz(quetz_deployment):
+    return True
+
+@pytest.fixture
+def plugin_dir(cookies):
     """Create a new plugin via cookiecutter and install it."""
     result = cookies.bake(extra_context={'plugin_name': 'foo_bar'})
 
@@ -65,9 +68,14 @@ def plugin(cookies):
     assert result.project.join('quetz_foo_bar/main.py').isfile()
     assert result.project.join('tests', 'test_main.py').isfile()
 
-    try:
-        subprocess.check_call(["python", "-m", "pip", "install", "-e", result.project])
-        yield str(result.project)
-    except subprocess.CalledProcessError as e:
-        pytest.fail(e)
-    subprocess.check_call(["pip", "uninstall", "foo_bar", "-y"])
+    return result.project
+
+@pytest.fixture
+def install_plugin(plugin_dir, use_quetz):
+    if use_quetz:
+        quetz_cmd("plugin", "install", plugin_dir)
+    else:
+        shell_cmd("pip", "install", "-e", plugin_dir)
+
+    yield plugin_dir
+    shell_cmd("pip", "uninstall", "quetz-foo_bar", "-y")
